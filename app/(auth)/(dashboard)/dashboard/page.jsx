13c047278cc68e506e55219220e662ac81ff6f0b
Om Pandey <mynameisompandey007@gmail.com>
2024-09-14 22:12:03 +0530

    some changes

--- app/(auth)/(dashboard)/dashboard/page.jsx
+++ app/(auth)/(dashboard)/dashboard/page.jsx
@@ -2,20 +2,19 @@
 import Navbar from "@/components/Navbar";
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { BookOpenCheck } from "lucide-react";
 
 const Dashboard = () => {
   return (
-    <div className="w-full h-screen p-4">
-      <div className="w-full flex p-2 bg-zinc-800 justify-between rounded-md">
+    <div className="w-full h-screen p-4 ">
+      <div className="w-full flex py-2 px-4 bg-zinc-800 items-center justify-between rounded-md">
+        <BookOpenCheck />
         <Navbar />
         <div className="flex gap-2">
-        <Button variant='ghost'>Signup</Button>
-        <Button variant='ghost'>Login</Button>
+          <Button variant="ghost">Signup</Button>
+          <Button variant="ghost">Login</Button>
         </div>
-
       </div>
-      
-
     </div>
   );
 };
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,41 +14,40 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
-
  
 const components= [
   {
     title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
+    href: "",
     description:
       "A modal dialog that interrupts the user with important content and expects a response.",
   },
   {
     title: "Hover Card",
-    href: "/docs/primitives/hover-card",
+    href: "",
     description:
       "For sighted users to preview content available behind a link.",
   },
   {
     title: "Progress",
-    href: "/docs/primitives/progress",
+    href: "",
     description:
       "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
   },
   {
     title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
+    href: "",
     description: "Visually or semantically separates content.",
   },
   {
     title: "Tabs",
-    href: "/docs/primitives/tabs",
+    href: "",
     description:
       "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
   },
   {
     title: "Tooltip",
-    href: "/docs/primitives/tooltip",
+    href: "",
     description:
       "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
   },
@@ -64,7 +63,7 @@ const Navbar = () => {
             <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
                 <NavigationMenuLink asChild>
-                  <Link
+                  <a
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                     href="/"
                   >
@@ -77,16 +76,16 @@ const Navbar = () => {
                       paste into your apps. Accessible. Customizable. Open
                       Source.
                     </p>
-                  </Link>
+                  </a>
                 </NavigationMenuLink>
               </li>
-              <ListItem href="/docs" title="Introduction">
+              <ListItem href="" title="Introduction">
                 Re-usable components built using Radix UI and Tailwind CSS.
               </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
+              <ListItem href="" title="Installation">
                 How to install dependencies and structure your app.
               </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
+              <ListItem href="" title="Typography">
                 Styles for headings, paragraphs, lists...etc
               </ListItem>
             </ul>
@@ -123,11 +122,11 @@ const Navbar = () => {
 
 export default Navbar
 
-const ListItem = ({ className, title, children, ...props }, ref) => {
+const ListItem =( { className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <Link
+        <a
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -139,7 +138,7 @@ const ListItem = ({ className, title, children, ...props }, ref) => {
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </Link>
+        </a>
       </NavigationMenuLink>
     </li>
   )
