32c3391b680226f503e0f8e3d0cfaf08a1e0c09b
Om Pandey <mynameisompandey007@gmail.com>
2024-11-02 11:17:44 +0530

    some adjustments

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -10,18 +10,15 @@ import FileUpload from "@/components/Uploader";
 
 const Dashboard = () => {
   return (
-    <div className="min-h-screen bg-black p-4">
+    <div className="min-h-screen bg-black p-4 flex flex-col gap-3">
       <div className="w-full backdrop-blur-sm bg-zinc-900/70 rounded-xl border border-zinc-800/50 shadow-xl">
         <div className="w-full h-16 flex items-center px-6">
-          
           {/* Left Section - Fixed width */}
           <div className="w-48 flex items-center gap-2">
             <BookOpenCheck className="h-6 w-6 text-white" />
-            <span className="text-lg font-semibold text-white">
-              EduPortal
-            </span>
+            <span className="text-lg font-semibold text-white">EduPortal</span>
           </div>
-          
+
           {/* Center Section (Navbar) - Exact center */}
           <div className="flex-1 flex justify-center items-center">
             <div className="absolute left-1/2 -translate-x-1/2">
@@ -38,12 +35,12 @@ const Dashboard = () => {
         </div>
       </div>
 
-      <div className="mt-6 p-6 backdrop-blur-sm bg-zinc-900/30 rounded-xl border border-zinc-800/50">
-        <h1 className="text-2xl font-semibold text-white mb-4">
+      <div className=" p-6 backdrop-blur-sm bg-zinc-800/50 rounded-xl border border-zinc-800/50">
+        <h1 className="text-2xl font-semibold text-white ">
           Welcome to Dashboard
         </h1>
       </div>
-      <FileUpload/>
+      <FileUpload />
     </div>
   );
 };
